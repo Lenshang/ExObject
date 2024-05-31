68ceff14956c36fbf9720905bbddb738416e13c3
LenShang <lenshang@qq.com>
2024-05-31 14:36:16 +0800

    update

--- setup.py
+++ setup.py
@@ -4,36 +4,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='exobject',
-    version=0.5,
-    description=(
-        'Extend Object Code'
-    ),
-    long_description=open('README.rst').read(),
-    author='LenShang',
-    author_email='lenshang@qq.com',
-    maintainer='LenShang',
-    maintainer_email='lenshang@qq.com',
-    license='BSD License',
+    name="exobject",
+    version=0.9,
+    description=("Extend Object Code"),
+    long_description=open("README.rst").read(),
+    author="LenShang",
+    author_email="lenshang@qq.com",
+    maintainer="LenShang",
+    maintainer_email="lenshang@qq.com",
+    license="BSD License",
     packages=find_packages(),
     platforms=["all"],
-    url='',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries'
-    ],
-    install_requires=[
-        'python-dateutil'
-    ]
+    url="",
+    install_requires=["python-dateutil", "parsel"],
 )
--- test.py
+++ test.py
@@ -1,5 +1,23 @@
-from ExObject.ExStock import is_trading_day,get_last_trading_day,get_next_trading_day
-from ExObject.DateTime import DateTime
-print(is_trading_day(DateTime.Now()))
-print(get_last_trading_day(DateTime.Now()))
-print(get_next_trading_day(DateTime.Now()))
+# from ExObject.ExStock import is_trading_day, get_last_trading_day, get_next_trading_day, get_price
+# from ExObject.DateTime import DateTime
+from ExObject.ExParsel import ExSelector
+import requests
+
+# print(is_trading_day(DateTime.Now()))
+# print(get_last_trading_day(DateTime.Now()))
+# print(get_next_trading_day(DateTime.Now()))
+
+# data=get_price("300059.SZ","2021-11-15")
+
+# data2=get_price("300059.SZ","2018-11-13","cn")
+# print(data)
+# print(data2)
+
+
+res = requests.get("https://www.newsbtc.com/").text
+data = ExSelector(res)
+for item in data.xpath("//article[contains(@class,'block-article')]"):
+    source_url = item.xpath(".//a/@href").FIrstCleanString()
+    title = item.xpath(".//a//text()").FIrstCleanString()
+    cover_pic = item.xpath(".//img/@src").extract().AllCleanString()
+    print(cover_pic)
